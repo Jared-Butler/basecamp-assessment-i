f4b604a1f04b86f8af2393c0d5474d139ed580e7
Jared Butler <jaredbtlr21@gmail.com>
2018-08-09 22:48:56 -0600

    I changed something when I hit save, must have gotten a key.

--- assessment.js
+++ assessment.js
@@ -4,7 +4,7 @@ var myName = 'Jared Butler';
 
 // #2 create a variable called 'faveNum' and assign it to your favorite number
 
-Var faveNum = '21';
+var faveNum = '21';
 
 // #3 create a variable called 'lovesCode' and set it to true
 
@@ -16,7 +16,7 @@ var me = "My name is "+myName+" and my favorite number is "+faveNum;
 
 // #5 Using the previously created faveNum variable, create a new variable called 'trueFaveNum'. *trueFaveNum* should equal true when comparing faveNum to your favorite number.v // HINT: Use a comparison operator
 
-var trueFaveNum == faveNum;
+var trueFaveNum = faveNum;
 
 // #6 Create a new variable called notHakeem. Using the previously created myName variable, test that your name DOES NOT equal the string 'Hakeem Olajuwon'. If your name does happen to be Hakeem Olajuwon, test against 'Karl Malone'
 
@@ -26,8 +26,8 @@ var notHakeem = (myName.length == "Hakeem Olajuwon".length);
 var age = 18;
 var canDrive;
 
+var canDrive = if age > 16 return "True":
 
-// CODE HERE
 
 // #8 Create an if statement inside of the responseCreator function that sets the colorResponse variable value based off of a passed in value. The param value passed in will be added in the unit test. If the param value is 'green' set colorResponse variable to 'Green is okay'. If the param value is 'red' set reponse variable to 'I heart red'. Otherwise set the colorResponse variable to 'What is your favorite color?'. Make sure to return the colorResponse variable at the end once it has been properly set.
 
