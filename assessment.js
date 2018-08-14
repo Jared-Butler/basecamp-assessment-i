660ad35396b212279589d484f4f70f1e5ba30deb
Jared Butler <jaredbtlr21@gmail.com>
2018-08-14 16:56:45 -0600

    Pushing this assesment after completing the task to do it in Canvas.

--- assessment.js
+++ assessment.js
@@ -1,10 +1,10 @@
 // #1 create a variable called 'myName' and assign it to your name as a string
 
-var myName = 'Jared Butler';
+var myName = "Jared Butler";
 
 // #2 create a variable called 'faveNum' and assign it to your favorite number
 
-var faveNum = '21';
+var faveNum = 21;
 
 // #3 create a variable called 'lovesCode' and set it to true
 
@@ -21,12 +21,16 @@ var trueFaveNum = faveNum;
 // #6 Create a new variable called notHakeem. Using the previously created myName variable, test that your name DOES NOT equal the string 'Hakeem Olajuwon'. If your name does happen to be Hakeem Olajuwon, test against 'Karl Malone'
 
 var notHakeem = (myName.length == "Hakeem Olajuwon".length);
+console.log(notHakeem);
+
+var notHakeem = (myName.length == "Karl Malone".length);
+console.log(notHakeem);
 
 // #7 Create an if statement that checks the age variable below. If age is greater than 16, then set canDrive to true otherwise set canDrive to false. The canDrive variable will be set by the unit test so do not adjust it.
 var age = 18;
 var canDrive;
 
-var canDrive = if age > 16 return "True":
+var canDrive = age > 16;
 
 
 // #8 Create an if statement inside of the responseCreator function that sets the colorResponse variable value based off of a passed in value. The param value passed in will be added in the unit test. If the param value is 'green' set colorResponse variable to 'Green is okay'. If the param value is 'red' set reponse variable to 'I heart red'. Otherwise set the colorResponse variable to 'What is your favorite color?'. Make sure to return the colorResponse variable at the end once it has been properly set.
@@ -34,13 +38,24 @@ var canDrive = if age > 16 return "True":
 var colorResponse;
 
 var responseCreator = function(param){
-  // CODE HERE
-}
+ if (param == "green"){console.log("Green is okay")}
+ else if (param == "red"){console.log("I heart red")}
+ else {console.log("What is your favorite color?")};
+};
 
 // #9 Create a function called 'sum' that takes in one parameter and returns the sum of that parameter added it itself.
 
-// CODE HERE
+function sum(param1){
+  console.log(param1+param1)
+};
+
 
 // #10 Create a function called 'greeter' that takes in two parameters, name and number. Have 'greeter' return a string that says something like this 'Hi! I am Bryan and I am 28 years old" but with the appropriate values..
 
-// CODE HERE
+var name = "Jared Butler";
+var number = 24
+
+function greeter(name,number){
+  console.log("Hi, my name is "+name+" and I am "+number+" years old!")
+};
+greeter(name, number)
